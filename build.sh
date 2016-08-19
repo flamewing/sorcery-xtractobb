3c483a006e0e6604cada6b1faf6dbe58f40f6b7c
flamewing <flamewing.sonic@gmail.com>
2016-08-19 08:38:56 -0300

    Using C++14. Revriting build script to allow debugging and profiling.

--- build.sh
+++ build.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
-g++ -O3 -s -Wall -Wextra -std=c++11 -o xtractobb xtractobb.cc jsont.cc -lboost_system -lboost_filesystem -lboost_iostreams -lboost_regex
-g++ -O3 -s -Wall -Wextra -std=c++11 -o pretty-print-json pretty-print-json.cc jsont.cc -lboost_system -lboost_filesystem -lboost_iostreams
+#!/bin/bash
+
+CXXFLAGS="-g3 -Wall -Wextra -std=c++14"
+DEBUG_FLAGS=
+LIBS="-lboost_system -lboost_filesystem -lboost_iostreams -lboost_regex"
+
+if [[ "$1" == "debug" ]]; then
+	DEBUG_FLAGS="-O0 -g3"
+elif [[ "$1" == "gprof" ]]; then
+	DEBUG_FLAGS="-O3 -g"
+else
+	DEBUG_FLAGS="-O3 -s"
+fi
+
+rm *~
+g++ $CXXFLAGS $DEBUG_FLAGS -o xtractobb xtractobb.cc jsont.cc $LIBS
+g++ $CXXFLAGS $DEBUG_FLAGS -o pretty-print-json pretty-print-json.cc jsont.cc $LIBS
 
