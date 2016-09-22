07cb24aff9b183b1528abe026eff53ceaf62b03f
flamewing <flamewing.sonic@gmail.com>
2016-09-22 20:13:35 -0300

    Adding Sorcery! 4 support.

--- xtract_all_obbs.sh
+++ xtract_all_obbs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Delete everything
-rm -rf sorcery{1,2,3}{obb,json}
+rm -rf sorcery{1,2,3,4}{obb,json}
 
 # Extract Sorcery! 1 files
 ./xtractobb com.inkle.sorcery1/main.1200.com.inkle.sorcery1.obb sorcery1obb/
@@ -11,8 +11,11 @@ rm -rf sorcery{1,2,3}{obb,json}
 # Extract Sorcery! 3 files
 ./xtractobb com.inkle.sorcery3/main.1030.com.inkle.sorcery3.obb sorcery3obb/
 
-mkdir -p sorcery{1,2,3}json/FightScenes
-for linkdir in 1 2 3; do
+# Extract Sorcery! 4 files
+./xtractobb com.inkle.sorcery4/main.1000.com.inkle.sorcery4.obb sorcery4obb/
+
+mkdir -p sorcery{1,2,3,4}json/FightScenes
+for linkdir in 1 2 3 4; do
 	find sorcery${linkdir}obb -iname '*.json' -or -iname '*.inkcontent' | while read ff; do
 		link=sorcery${linkdir}json/${ff#sorcery${linkdir}obb/}
 		ln -fns `pwd`/$ff $link
