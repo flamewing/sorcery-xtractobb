11452e29b936829e74b3abade745220d2dc4e651
flamewing <flamewing.sonic@gmail.com>
2018-12-24 11:46:41 +0100

    Updating to latest obb file names
    Also, moving output to 'output' directory.

--- xtract_all_obbs.sh
+++ xtract_all_obbs.sh
@@ -3,22 +3,26 @@
 rm -rf sorcery{1,2,3,4}{obb,json}
 
 # Extract Sorcery! 1 files
-./xtractobb com.inkle.sorcery1/main.1200.com.inkle.sorcery1.obb sorcery1obb/
+./xtractobb com.inkle.sorcery1/main.14002.com.inkle.sorcery1.obb output/sorcery1obb/
 
 # Extract Sorcery! 2 files
-./xtractobb com.inkle.sorcery2/main.1100.com.inkle.sorcery2.obb sorcery2obb/
+./xtractobb com.inkle.sorcery2/main.13002.com.inkle.sorcery2.obb output/sorcery2obb/
 
 # Extract Sorcery! 3 files
-./xtractobb com.inkle.sorcery3/main.1030.com.inkle.sorcery3.obb sorcery3obb/
+./xtractobb com.inkle.sorcery3/main.12002.com.inkle.sorcery3.obb output/sorcery3obb/
 
 # Extract Sorcery! 4 files
-./xtractobb com.inkle.sorcery4/main.1000.com.inkle.sorcery4.obb sorcery4obb/
+./xtractobb com.inkle.sorcery4/main.11002.com.inkle.sorcery4.obb output/sorcery4obb/
 
+pushd output
 mkdir -p sorcery{1,2,3,4}json/FightScenes
+
 for linkdir in 1 2 3 4; do
 	find sorcery${linkdir}obb -iname '*.json' -or -iname '*.inkcontent' | while read -r ff; do
 		link=sorcery${linkdir}json/${ff#sorcery${linkdir}obb/}
 		ln -fns $(pwd)/$ff $link
 	done
 done
+
+popd
 
